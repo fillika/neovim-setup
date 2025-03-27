e80928e8407b2aa75232d9be8db9680ccfbfb4a9
Aleksandr Mironenko <a.mironenko@initi.ru>
2025-03-28 00:24:04 +0300

    - added indents - added correct langmap for ru eng

--- nvim/lua/pioneer/options.lua
+++ nvim/lua/pioneer/options.lua
@@ -1,25 +1,31 @@
 local options = {
-    number = true,
-    relativenumber = true,
-    smartcase = true,
-    ignorecase = true,
-    shiftwidth = 4,
-    tabstop = 4,
-    softtabstop = 4,
-    expandtab = true,
-    showtabline = 2,
-    cmdheight = 2,
-    fileencoding = "utf-8",
-    mouse = "a",
-    updatetime = 500,
-    splitbelow = true,
-    splitright = true,
-    wrap = false,
-    cursorline = true,
-    termguicolors = true,
-    guifont = "ubuntu:h20",
-    guicursor = "n:block,v:ver35,i:ver35",
-    langmap = "фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz,ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	number = true,
+	relativenumber = true,
+	smartcase = true,
+	ignorecase = true,
+	shiftwidth = 4,
+	tabstop = 4,
+	softtabstop = 4,
+	expandtab = true,
+	showtabline = 2,
+	cmdheight = 2,
+	fileencoding = "utf-8",
+	mouse = "a",
+	updatetime = 500,
+	splitbelow = true,
+	splitright = true,
+	wrap = false,
+	cursorline = true,
+	termguicolors = true,
+	guifont = "ubuntu:h20",
+	guicursor = "n:block,v:ver35,i:ver35",
+	langmap =
+	"йq,цw,уe,кr,еt,нy,гu,шi,щo,зp,х[,ъ]," ..
+	"фa,ыs,вd,аf,пg,рh,оj,лk,дl,ж',э\\," ..
+	"яz,чx,сc,мv,иb,тn,ьm,б.,ю/," ..
+	"ЙQ,ЦW,УE,КR,ЕT,НY,ГU,ШI,ЩO,ЗP,Х{,Ъ}," ..
+	"ФA,ЫS,ВD,АF,ПG,РH,ОJ,ЛK,ДL,Ж\",Э|," ..
+	"ЯZ,ЧX,СC,МV,ИB,ТN,ЬM,Б<,Ю>",
 }
 
 for k, v in pairs(options) do
--- nvim/lua/pioneer/plugins/telescope.lua
+++ nvim/lua/pioneer/plugins/telescope.lua
@@ -1,5 +1,16 @@
 return {
-    'nvim-telescope/telescope.nvim', 
-	tag = '0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim' }
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function ()
+        require("telescope").setup({
+            defaults = {
+                layout_config = {
+                    preview_width = 0.65,
+                    width = 0.9,
+                    height = 0.9,
+                },
+            },
+        })
+    end
 }
