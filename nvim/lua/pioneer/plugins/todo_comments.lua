4caa38143daea121e079db31ba9c1fb41035b38b
Aleksandr Mironenko <a.mironenko@initi.ru>
2025-04-04 08:25:15 +0300

    - todo comments

--- nvim/lua/pioneer/plugins/todo_comments.lua
+++ nvim/lua/pioneer/plugins/todo_comments.lua
@@ -1,36 +1,45 @@
 return {
-	"folke/todo-comments.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		require("todo-comments").setup({
-			-- Настройка ключевых слов и их цветов
-			keywords = {
-				fix = {
-					icon = " ", -- Иконка для FIX
-					color = "error", -- Цвет для FIX
-					alt = { "FIXME", "BUG", "bug","FIXIT", "ISSUE" }, -- Альтернативные ключевые слова
-				},
-				todo = { icon = " ", color = "chocolate", alt = { "TODO" } },
-				hack = { icon = " ", color = "warning" },
-				warn = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-				perf = { icon = " ", color = "hint", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-				note = { icon = " ", color = "hint", alt = { "INFO" } },
-			},
-			-- Настройка цветов
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        require("todo-comments").setup({
+            -- Настройка ключевых слов и их цветов
+            keywords = {
+                fix = {
+                    icon = " ",
+                    color = "#FF0000",
+                    alt = { "FIX", "FIXME", "BUG", "bug", "FIXIT", "issue", "ISSUE" }, -- Альтернативные ключевые слова
+                },
+                todo = { icon = " ", color = "chocolate", alt = { "TODO" } },
+                hack = { icon = " ", color = "warning" },
+                warn = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+                perf = { icon = " ", color = "hint", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+                note = {
+                    icon = " ", -- Иконка карандаша в стиле Nerd Fonts
+                    color = "greenSea",
+                    alt = { "NOTE", "info", "INFO" },
+                },
+            },
+            colors = {
+                greenSea = "#2E8B57", -- Зеленое море
+                chocolate = "#D2691E",
+                error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+                warning = { "DiagnosticWarning", "WarningMsg", "#FBBF24" },
+                info = { "DiagnosticInfo", "#2563EB" },
+                hint = { "DiagnosticHint", "#10B981" },
+                default = { "Identifier", "#7C3AED" },
+            },
+            highlight = {
+                pattern = [[.*(\/\/|#|--|\*)\s*(KEYWORDS)\s*]],
+            },
+            -- Настройка цветов
+            -- todo  Hello without :
             -- todo: Hello
             -- hack: Hello
             -- warn: Hello
             -- perf: Hello
             -- note: Hello
             -- fix: Hello
-			colors = {
-                chocolate = { "NiceChocolate", "#D2691E" },
-				error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
-				warning = { "DiagnosticWarning", "WarningMsg", "#FBBF24" },
-				info = { "DiagnosticInfo", "#2563EB" },
-				hint = { "DiagnosticHint", "#10B981" },
-				default = { "Identifier", "#7C3AED" },
-			},
-		})
-	end,
+        })
+    end,
 }
